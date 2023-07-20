321943f88e3b26e7ff4346ca868c8a63beda5c1a
tidiane004@gmail.com <btidiane004@gmail.com>
2023-07-20 12:30:11 +0100

    Ajout de copy.py dans la branch develop

---
+++ copy.py
@@ -0,0 +1,8 @@
+# Fonction qui calcule la somme de deux nombres
+def somme_deux_nombres(nombre1, nombre2):
+    resultat = nombre1 + nombre2
+    return resultat
+
+# Exemple d'utilisation de la fonction
+somme = somme_deux_nombres(5, 3)
+print(somme)  # Cela affichera 8
